ae7254b4082e9dd12db0276ff9b312c46740ca74
Nishant-96 <nishant11pratihast@gmail.com>
2021-08-06 19:27:28 +0530

    Update: app functionality done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var purchasePrice = document.querySelector("#purchase-price");
 var stockQuantity = document.querySelector("#stock-quantity");
 var currentPrice = document.querySelector("#current-price");
 var checkButton = document.querySelector("#check-button");
+var outputBox = document.querySelector("#output-box");
+var errorBox = document.querySelector("#error-box");
 
 
 
@@ -11,22 +13,37 @@ checkButton.addEventListener("click", checkProfitOrLoss )
 
 function checkProfitOrLoss(){
 
-    // var costPrice=purchasePrice.value;
-    // var quantity=stockQuantity.value;
-    // var stockPrice=currentPrice.value;
+    var costPrice=Number(purchasePrice.value);
+    var quantity=Number(stockQuantity.value);
+    var stockPrice=Number(currentPrice.value);
 
-    if(purchasePrice.value > 0 && stockQuantity.value > 0 && currentPrice.value> 0 ){
-        //Loss 
-       if(purchasePrice.value > currentPrice.value){
-         var lossAmount=(purchasePrice.value- currentPrice.value)*stockQuantity.value;
-         var lossPercentage=((purchasePrice.value-currentPrice.value)/purchasePrice.value)*100  ;
-         console.log(lossPercentage,lossAmount);
-       }
-        
-    }else   {
-        console.log("Error : Enter valid values (i.e. positive values) in all the fields.")
+    errorBox.style.display = "none";
+    outputBox.style.display = "none";
+
+    if( costPrice > 0 && quantity > 0 && stockPrice> 0 ){
+        //loss    
+        if(costPrice>stockPrice){
+            var lossAmount = (costPrice-stockPrice)*quantity;
+            var lossPercentage = ((costPrice-stockPrice)/costPrice)*100;
+            
+            outputBox.style.display = "block";
+            outputBox.innerHTML=  `<div style="background-color: rgb(255, 255,255,0.2); padding: 0.5rem; margin-top:1.5rem">You lost ${lossPercentage}%. Your total loss is ₹${lossAmount}</div>`
+            }
+            //profit
+            else if(costPrice < stockPrice ) {
+
+            var profitAmount = (stockPrice-costPrice)*quantity;
+            var profitPercentage = ((stockPrice-costPrice)/costPrice)*100;
+            
+            outputBox.style.display = "block";
+            outputBox.innerHTML= `<div style="background-color: rgb(255, 255,255,0.2); padding: 0.5rem; margin-top:1.5rem">You gained ${profitPercentage}%. Your total profit is ₹${profitAmount}</div>`
+            }else{
+
+                outputBox.style.display = "block";
+                outputBox.innerHTML=`<div style="background-color: rgb(255, 255,255,0.2); padding: 0.5rem; margin-top:1.5rem">You Stock Price is ₹${stockPrice} same as Purchase price ₹${costPrice}. You neither lost nor gained any money.</div>`
+            }
+    }else {
+        errorBox.style.display = "block";
+        errorBox.innerHTML="Please enter values greater than 0 in all the above fields (only numbers are allowed).";
     }
-    
-
-   
 }
